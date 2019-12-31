70639360d05b5f04ad068401dd15266b50fcc838
alansarluv <alan.sarluf@gmail.com>
2019-12-31 11:48:50 +0700

    Allow CORS

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+  next();
+})
+
 app.use(atecRoutes);
 app.use(userRoutes);
 
